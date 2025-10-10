37f1c6210e0a33280a2ff92f9299d9efa0a1ea99
Vaibhav Pandey <vaibhavkpandey7@gmail.com>
2025-10-10 20:10:38 +0530

    removed type errors during build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production'
-const basePath = isProd ? '/' : ''
+const basePath = ''
 
 const nextConfig = {
   // output: 'export',
