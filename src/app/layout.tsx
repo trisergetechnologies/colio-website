2357489ca50c2b43ff0e219e0e75433a776eaffd
Ojas kumar mishra <Ojasmishra525@gmail.com>
2026-02-21 03:57:53 +0530

    msandsand

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,36 +6,53 @@ import { AuthProvider } from '@/context/AuthContext'
 import { CallProvider } from '@/context/CallContext'
 import CallManager from './components/call/CallManager'
 import { ChatProvider } from '@/context/ChatContext'
+import Script from 'next/script'   // 👈 ADD THIS
+
 const font = Inter({ subsets: ['latin'] })
 
 export const dancingScript = Dancing_Script({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"], // logo flexibility
+  weight: ["400", "500", "600", "700"],
 });
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <html lang='en' suppressHydrationWarning>
-      <AuthProvider>
-        <CallProvider>
-          <ChatProvider>
-            <body className={`${font.className}`}>
+      
+      <head>
+        {/* Google Tag */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-MMP5LBSGSP"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-MMP5LBSGSP');
+          `}
+        </Script>
+      </head>
 
+      <body className={`${font.className}`}>
+        <AuthProvider>
+          <CallProvider>
+            <ChatProvider>
               <Aoscompo>
-
                 {children}
                 <CallManager />
-
               </Aoscompo>
               <ScrollToTop />
-            </body>
-          </ChatProvider>
-        </CallProvider>
-      </AuthProvider>
-    </html> 
+            </ChatProvider>
+          </CallProvider>
+        </AuthProvider>
+      </body>
+
+    </html>
   )
 }
